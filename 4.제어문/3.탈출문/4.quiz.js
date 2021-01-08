2fd377080819432fb9219c400fdb47882b99bee0
dlswn5645@naver.com <dlswn5645@naver.com>
2021-01-08 18:26:30 +0900

    [21.01.08]#js qiuz 숙제 완료

--- 4.제어문/3.탈출문/4.quiz.js
+++ 4.제어문/3.탈출문/4.quiz.js
@@ -1,19 +1,20 @@
 
 
-var firstNumber = Math.floor(Math.random() * 100);
-var secondNumber = Math.floor(Math.random() * 100);
-
 var correctCount = 0;
 var errorCount = 0;
 
-for ( var table = 0; table <= 100; table++ ){
+ while (true){
+    var firstNumber = Math.floor(Math.random() * 101);
+    var secondNumber = Math.floor(Math.random() * 101);
     var table = +prompt(`${firstNumber} + ${secondNumber} = ???`);
-    if( table === firstNumber + secondNumber){
-        alert('정답입니다.');
-    } else if ( table !== firstNumber + secondNumber ){
-        alert('오답입니다.');
-    } else if ( table === 0){
-        alert();
+    if( table === 0){
+        alert(`정답 횟수는 ${correctCount} , 오답 횟수는 ${errorCount}`);
         break;
+    } else if ( table === firstNumber + secondNumber ){
+        alert('정답입니다.');
+        correctCount++;
+    } else if ( table !== firstNumber + secondNumber){
+        alert('오답입니다.');
+        errorCount++;
     }
 }
